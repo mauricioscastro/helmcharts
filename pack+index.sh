4d5b116ef9853daffc0409eb293bc0bc49eda036
Mauricio Castro <mauricioscastro@hotmail.com>
2026-07-13 10:00:40 -0300

    script to package

---
+++ pack+index.sh
@@ -0,0 +1,4 @@
+ls | grep -v -e charts -e README -e index.yaml -e helmfiles -e pack+index | while read p; do 
+    helm package -u -d charts/ $p
+done; 
+helm repo index . --url https://charts.mscastro.net
