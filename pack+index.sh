2e6aa6bafc2604cd8c699e2036378bfcdceb6bab
Mauricio Castro <mauricioscastro@hotmail.com>
2026-07-13 11:26:41 -0300

    update main

--- pack+index.sh
+++ pack+index.sh
@@ -3,3 +3,12 @@ ls | grep -v -e charts -e README -e index.yaml -e helmfiles -e pack+index | whil
     helm package -u -d charts/ $p
 done; 
 helm repo index . --url https://charts.mscastro.net
+git add -A
+git commit -a -m "update main"
+git push
+git checkout gh-pages
+git restore --source main index.yaml charts/ helmfiles/ README.md
+git add -A
+git commit -a -m "update repo"
+git push
+git checkout main
